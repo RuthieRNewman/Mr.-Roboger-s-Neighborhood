5c18fd1047cf9c81c8de000ef8e19eac93ba4549
Ruthie Newman <ruthienewman@protonmail.com>
2020-06-12 15:40:03 -0700

    add else/if statements for each number in a 1-10 possibility range with specified outputs for each possibility.

--- js/scripts.js
+++ js/scripts.js
@@ -16,29 +16,69 @@ $(document).ready(function() {
     let mrRobogersResponse = "won't you be my neighbor?"
    console.log(mrRobogersResponse);
     return mrRobogersResponse;
-   $('#outputNumber').val(mrRobogersResponse);
+   //$("#output").append("<li>" + mrRobogersResponse + "</li>");
 
     }else if
       ((userArray.includes("3") === false) && (userArray.includes("2") === true)){
       let mrRobogersResponse = "Boop!"
       console.log(mrRobogersResponse);
       return mrRobogersResponse;
-      $('#outputNumber').val(mrRobogersResponse);
+      //$("#mrRobogersResponse").val(mrRobogersResponse);
 
     }else if
     ((userArray.includes("3") === false) && (userArray.includes("2") === false) && (userArray.includes("1") === true)) {
     let mrRobogersResponse = "Beep!"
     console.log(mrRobogersResponse);
     return mrRobogersResponse;
-  }else if 
-  ((userArray.includes("3") === false) && (userArray.includes("2") === false) && (userArray.includes("1") === false)) {
-    let userString = userArray.join();
-    let userFinalNumber = parseInt(userString);
-    console.log(userString);
-    //for (let index= userString; index <= userString; index += userString) {
-      //console.log(index);
+
+    }else if 
+    ((userArray.includes("3") === false) && (userArray.includes("2") === false) && (userArray.includes("1") === false) && (userArray.includes("4") === true)) {
+      let mrRobogersResponse = ["Beep!", "Boop!", "won't you be my neighbor?", 4];
+      console.log(mrRobogersResponse);
+      return mrRobogersResponse;
+
+    }else if 
+    ((userArray.includes("3") === false) && (userArray.includes("2") === false) && (userArray.includes("1") === false) && (userArray.includes("4") === false) && (userArray.includes("5") === true)) {
+      let mrRobogersResponse = ["Beep!", "Boop!", "won't you be my neighbor?", 4, 5];
+      console.log(mrRobogersResponse);
+      return mrRobogersResponse;
+    
+    }else if 
+    ((userArray.includes("3") === false) && (userArray.includes("2") === false) && (userArray.includes("1") === false) && (userArray.includes("4") === false) && (userArray.includes("5") === false) && (userArray.includes("6") === true)) {
+      let mrRobogersResponse = ["Beep!", "Boop!", "won't you be my neighbor?", 4, 5, 6];
+      console.log(mrRobogersResponse);
+      return mrRobogersResponse;
+    }else if 
+    ((userArray.includes("3") === false) && (userArray.includes("2") === false) && (userArray.includes("1") === false) && (userArray.includes("4") === false) && (userArray.includes("5") === false) 
+    && (userArray.includes("6") === false) && (userArray.includes("7") === true)) {
+    let mrRobogersResponse = ["Beep!", "Boop!", "won't you be my neighbor?", 4, 5, 6, 7];
+    console.log(mrRobogersResponse);
+    return mrRobogersResponse;
+    }else if 
+    ((userArray.includes("3") === false) && (userArray.includes("2") === false) && (userArray.includes("1") === false) && (userArray.includes("4") === false) && (userArray.includes("5") === false) 
+    && (userArray.includes("6") === false) && (userArray.includes("7") === false) && (userArray.includes("8") === true)) {
+    let mrRobogersResponse = ["Beep!", "Boop!", "won't you be my neighbor?", 4, 5, 6, 7, 8];
+    console.log(mrRobogersResponse);
+    return mrRobogersResponse;
+    }else if 
+    ((userArray.includes("3") === false) && (userArray.includes("2") === false) && (userArray.includes("1") === false) && (userArray.includes("4") === false) && (userArray.includes("5") === false) 
+    && (userArray.includes("6") === false) && (userArray.includes("7") === false) && (userArray.includes("8") === false) && (userArray.includes("9") === true)) {
+    let mrRobogersResponse = ["Beep!", "Boop!", "won't you be my neighbor?", 4, 5, 6, 7, 8, 9];
+    console.log(mrRobogersResponse);
+    return mrRobogersResponse;
+    }else if 
+    ((userArray.includes("3") === false) && (userArray.includes("2") === false) && (userArray.includes("1") === false) && (userArray.includes("4") === false) && (userArray.includes("5") === false) 
+    && (userArray.includes("6") === false) && (userArray.includes("7") === false) && (userArray.includes("8") === false) && (userArray.includes("9") === false) && (userArray.includes("10") === true)) {
+    let mrRobogersResponse = ["Beep!", "Boop!", "won't you be my neighbor?", 4, 5, 6, 7, 8, 9, 10];
+    console.log(mrRobogersResponse);
+    return mrRobogersResponse;
     }
-    }
-  }
+
+
+
+}
+}   
 });
 });
+
+//for let index= 0; index <= userFinalNumber.length; index ++
