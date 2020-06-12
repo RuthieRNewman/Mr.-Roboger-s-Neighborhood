a00b2d71cf774fc54230cf78c44fbce0de3df5b7
Ruthie Newman <ruthienewman@protonmail.com>
2020-06-12 12:02:16 -0700

    add 'for loop' to iterate over user input to determine if it includes a 1, 2 or 3. Add 'if/else statment' to assign specific outputs for 1, 2, or 3 if present.

--- js/scripts.js
+++ js/scripts.js
@@ -4,18 +4,33 @@ $(document).ready(function() {
   const userNumber = ($("input#userNumber").val());
   $(".mrRobogersResponse").text(mrRobogersResponse(userNumber));
   
+  
   function mrRobogersResponse(userNumberInput) {
   let userArray = userNumberInput.split('');
   console.log(userArray)
   console.log(userArray.includes("3"));
-  
+
   for (i=0; i=userArray.length; i++){
-    if (userArray.includes("3") === true)
-   
+    if (userArray.includes("3") === true){
+    let mrRobogersResponse = "won't you be my neighbor?"
+   console.log(mrRobogersResponse);
+    return mrRobogersResponse;
+   $('#outputNumber').val(mrRobogersResponse);
 
+    }else if
+      ((userArray.includes("3") === false) && (userArray.includes("2") === true)){
+      let mrRobogersResponse = "Boop!"
+      console.log(mrRobogersResponse);
+      return mrRobogersResponse;
+      $('#outputNumber').val(mrRobogersResponse);
 
+    }else if
+    ((userArray.includes("3") === false) && (userArray.includes("2") === false) && (userArray.includes("1") === true)) {
+    let mrRobogersResponse = "Beep!"
+    console.log(mrRobogersResponse);
+    return mrRobogersResponse;
   }
-
+}
   }
-});
+  });
 });
