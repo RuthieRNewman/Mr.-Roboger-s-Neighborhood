88b49d805981761c7ca1e86cdd4946c49cf792dc
Ruthie Newman <ruthienewman@protonmail.com>
2020-06-21 13:31:59 -0700

    dry variables and comparison operator statements.

--- js/scripts.js
+++ js/scripts.js
@@ -9,63 +9,25 @@ $(document).ready(function() {
 
 
   
-  function mrRobogersResponse(userNumberInput) {
-  let userArray = userNumberInput.split('');
-  
-
-  for (i=0; i=userArray.length; i++){
-    if (userArray.includes("3") === true){
+  function mrRobogersResponse(userNumber) {
+  for (i=0; i=userNumber.length; i++){
+    if (userNumber.includes("3") === true) {
     let mrRobogersResponse = "Won't you be my neighbor?"
-    return mrRobogersResponse;
-
+ 
     }else if
-      ((userArray.includes("3") === false) && (userArray.includes("2") === true)){
+      (userNumber.includes("2") === true) {
       let mrRobogersResponse = "Boop!"
-      return mrRobogersResponse;
-
+      
     }else if
-    ((userArray.includes("3") === false) && (userArray.includes("2") === false) && (userArray.includes("1") === true)) {
+    (userNumber.includes("1") === true) {
     let mrRobogersResponse = "Beep!"
-    return mrRobogersResponse;
-
-    }else if 
-    ((userArray.includes("3") === false) && (userArray.includes("2") === false) && (userArray.includes("1") === false) && (userArray.includes("4") === true)) {
-      let mrRobogersResponse = ["Beep!" , "Boop!", "Won't you be my neighbor?", 4];
-      mrRobogersResponseJoined = mrRobogersResponse.join(', ');
-      return mrRobogersResponseJoined;
-
-    }else if 
-    ((userArray.includes("3") === false) && (userArray.includes("2") === false) && (userArray.includes("1") === false) && (userArray.includes("4") === false) && (userArray.includes("5") === true)) {
-      let mrRobogersResponse = ["Beep!", "Boop!", "Won't you be my neighbor?", 4, 5];
-      mrRobogersResponseJoined = mrRobogersResponse.join(', ');
-      return mrRobogersResponseJoined;
+  
+    }else 
+    for (index = countBy; index <= countTo; index += countBy) {
+      let countTo = [userNumber];
+      let countBy = parseInt("1");    
+      console.log(countTo)
+  }
+}
+  }
     
-    }else if 
-    ((userArray.includes("3") === false) && (userArray.includes("2") === false) && (userArray.includes("1") === false) && (userArray.includes("4") === false) && (userArray.includes("5") === false) && (userArray.includes("6") === true)) {
-      let mrRobogersResponse = ["Beep!", "Boop!", "Won't you be my neighbor?", 4, 5, 6];
-      return mrRobogersResponseJoined;
-    
-    }else if 
-    ((userArray.includes("3") === false) && (userArray.includes("2") === false) && (userArray.includes("1") === false) && (userArray.includes("4") === false) && (userArray.includes("5") === false) 
-    && (userArray.includes("6") === false) && (userArray.includes("7") === true)) {
-    let mrRobogersResponse = ["Beep!", "Boop!", "Won't you be my neighbor?", 4, 5, 6, 7];
-    mrRobogersResponseJoined = mrRobogersResponse.join(', ');
-    return mrRobogersResponseJoined;
-    
-    }else if 
-    ((userArray.includes("3") === false) && (userArray.includes("2") === false) && (userArray.includes("1") === false) && (userArray.includes("4") === false) && (userArray.includes("5") === false) 
-    && (userArray.includes("6") === false) && (userArray.includes("7") === false) && (userArray.includes("8") === true)) {
-    let mrRobogersResponse = ["Beep!", "Boop!", "Won't you be my neighbor?", 4, 5, 6, 7, 8];
-    mrRobogersResponseJoined = mrRobogersResponse.join(', ');
-    return mrRobogersResponseJoined;
-
-    }else if  
-    ((userArray.includes("3") === false) && (userArray.includes("2") === false) && (userArray.includes("1") === false) && (userArray.includes("4") === false) && (userArray.includes("5") === false) 
-    && (userArray.includes("6") === false) && (userArray.includes("7") === false) && (userArray.includes("8") === false) && (userArray.includes("9") === true)) {
-    let mrRobogersResponse = ["Beep!", "Boop!", "Won't you be my neighbor?", 4, 5, 6, 7, 8, 9];
-    mrRobogersResponseJoined = mrRobogersResponse.join(', ');
-    return mrRobogersResponseJoined;
-    
-    }
-  }   
-  }   
